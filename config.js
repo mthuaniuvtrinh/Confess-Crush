0ac35ea1140b9b3f5ca1b7b0f78502220d75cb99
mthuaniuvtrinh <99545045+mthuaniuvtrinh@users.noreply.github.com>
2022-02-12 21:49:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    titleWeb: "Anh cho em bé vt nèee",
+    introTitle: 'Bùi Việt Trinh',
+    introDesc: `Em bé đừng giận anh nữa nha`,
+    btnIntro: 'ỏoo anh chào bé iu nha',
+    title: 'embe có iu anh hong ạ',
+    desc: 'Bé có thể hong buồn anh nữa hong ',
+    btnYes: 'be iu anh',
+    btnNo: 'hong iu anh đâuuu',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại iu tớ',
+    btnReply: 'bé cho anh nà',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'anh cũng yêu embe nhiều lắm a',
+    messDesc: 'embe cho anh xin lỗi nhiều nha, bé tha lỗi cho anh',
+    btnAccept: 'anh hong cố ý làm embe buồn đâu a, anh xin lỗi bé',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
